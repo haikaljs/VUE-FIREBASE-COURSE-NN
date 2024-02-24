4326a04270ad079c71b439091b2e630f684190a1
haikaljs <haikaljavascript@gmail.com>
2024-02-24 13:06:00 +0800

    Templates and data

--- 2-VUE-BASICS/1-app.js
+++ 2-VUE-BASICS/1-app.js
@@ -1,6 +1,13 @@
 const app = Vue.createApp({
     // data, functions
-    template: '<h2>Hi Haikal, you want to learn Vuejs</h2>'
+    // template: '<h2>Hi Haikal, you want to learn Vuejs</h2>'
+    data(){
+        return {
+            title: 'The Haikal Cendol',
+            author: 'Haikal Rozhan',
+            age: 32
+        }
+    }
 })
 
 app.mount('#app ')  
