03f1225a4895d6e48c11e8b1726bd568bc881a0a
haikaljs <haikaljavascript@gmail.com>
2024-02-25 16:12:25 +0800

    Passing data with props

---
+++ 3-vue-cli/modal-two/src/App.vue
@@ -0,0 +1,31 @@
+<script>
+import Modal from "./components/Modal.vue"
+
+export default {
+    name: 'App',
+    components: {Modal},
+
+    data(){
+      return{
+        title: 'My first vue',
+        header: "Sign up for the giveaway",
+        text: "Text description"
+
+      }
+    }
+}
+</script>
+
+<template>
+ 
+<Modal :header="header" :text="text" theme="sale"/>
+
+</template>
+
+<style>
+*{
+  margin: 0;
+  padding: 0;
+}
+
+</style>
---
+++ 3-vue-cli/modal-two/src/components/Modal.vue
@@ -0,0 +1,44 @@
+<script>
+export default{
+    props: ['header', 'text','theme'],
+    data(){
+    return{
+        title: 'Modal Title'
+    }
+}
+}
+
+</script>
+
+<template>
+<div class="backdrop">
+    <div class="modal" :class="{sale : theme === 'sale'}">
+        <h1>{{header}}</h1>
+        <p class="modal-content">{{text}}</p>
+    </div>
+</div>
+</template>
+
+<style scoped>
+.backdrop{
+    background: rgba(0, 0, 0, 0.5);
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.modal{
+    /* background: white; */
+    width: 400px;
+    height: 400px;
+    padding: 10px;
+    background: white;
+    text-align: center;
+}
+.modal.sale{
+    background: crimson;
+    color: white;
+}
+
+</style>
