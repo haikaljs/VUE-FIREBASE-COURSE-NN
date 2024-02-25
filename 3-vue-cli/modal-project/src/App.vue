a1de1de99a2aa5f588ac4ee3f1e717ad3defe124
haikaljs <haikaljavascript@gmail.com>
2024-02-25 15:05:51 +0800

    vue files and template

--- 3-vue-cli/modal-project/src/App.vue
+++ 3-vue-cli/modal-project/src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <h1>{{ title }}</h1>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+
+  },
+  data(){
+    return {
+      title: 'My first app'
+    }
+    
   }
 }
 </script>
@@ -20,7 +25,11 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+
   margin-top: 60px;
+}
+h1{
+  color: white;
+  background: pink;
 }
 </style>
