3810d1b69bc6a7c5aa1a7ab85abb3e32ea9a066d
haikaljs <haikaljavascript@gmail.com>
2024-02-25 15:36:57 +0800

    Component styles and global styles

--- 3-vue-cli/modal-project/src/App.vue
+++ 3-vue-cli/modal-project/src/App.vue
@@ -1,44 +1,21 @@
 <template>
- <h1>{{ title }}</h1>
- <input type="text" ref="name">
- <button @click="handleClick">Click me</button>
-</template>
+  <Modal />
+ </template>
+
 
 <script>
-
+ /* eslint-disable */
+import Modal from './components/Modal.vue'
 
 export default {
   name: 'App',
-  components: {
-
-  },
-  data(){
-    return {
-      title: 'My first app'
-    }
-    
-  },
-  methods: {
-    handleClick(){
-      console.log(this.$refs.name);
-      this.$refs.name.classList.add('active')
-      this.$refs.name.focus()
-    }
-  }
+  components: {Modal}
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
 
-  margin-top: 60px;
-}
-h1{
-  color: white;
-  background: pink;
-}
+
+<style>
+
+
 </style>
